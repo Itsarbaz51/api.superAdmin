4f0df98f6a25508a00fe4863947290187c1a4f61
azzunique <azunisoftware18@gmail.com>
2025-10-14 11:29:48 +0530

    done

--- prisma/migrations/20251010103409_add_completed_at_to_transaction/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `settings` ALTER COLUMN `created_at` DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE `transactions` ADD COLUMN `completed_at` DATETIME(3) NULL;
--- prisma/migrations/20251011132443_make_user_kyc_id_nullable/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `business_kycs` ADD COLUMN `udhyam_aadhar` VARCHAR(191) NULL;
-
--- AlterTable
-ALTER TABLE `settings` ALTER COLUMN `created_at` DROP DEFAULT;
